2c90eb9a350d92ad9a3bcba519864d3156bd6969
Souvik Biswas <sbis1999@gmail.com>
2020-12-25 09:25:59 +0530

    Add morse test

---
+++ test/morse_test.dart
@@ -0,0 +1,19 @@
+import 'package:morse/scr/morse_converter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Fails with null', () {
+    expect(() => Morse.fromString(null), throwsArgumentError);
+  });
+
+  test('Convert to morse', () {
+    final m = Morse.fromString('hello world');
+    expect(m.textLength, 11);
+    expect(m.morseLength, 61);
+  });
+  test('Convert to normal text', () {
+    final m = Morse.toString('--.... --. --.-.. --.-.. -----/---- --.- --.. ---.');
+    expect(m.textLength, 10);
+    expect(m.morseLength, 50);
+  });
+}
