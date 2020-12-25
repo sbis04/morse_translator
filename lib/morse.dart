f2c241b46c873a6077869bb13477df96807faef7
Souvik Biswas <sbis1999@gmail.com>
2020-12-25 08:53:06 +0530

    export in one file

--- lib/morse.dart
+++ lib/morse.dart
@@ -1,7 +1,1 @@
-library custom_morse;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:morse/scr/morse_converter.dart';
